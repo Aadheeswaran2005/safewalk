5673c1bda4468cbbc98ec65812f2d1113641c2cc
Aadheeswaran2005 <720522104001@ecetonline.com>
2026-04-07 19:09:37 +0530

    Restore project files

---
+++ src/main/java/com/safewalk/safewalk/SafewalkApplication.java
@@ -0,0 +1,13 @@
+package com.safewalk.safewalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafewalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafewalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safewalk/safewalk/SafewalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safewalk.safewalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafewalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
